6be9e9951c5896b283e8a4069615a50a729fc45b
Bauntyy <t_chesnokov@bk.ru>
2025-12-08 18:34:32 +0400

    change Regular_Dodecahedron class and create_vertices function

--- lab_4/main.py
+++ lab_4/main.py
@@ -80,92 +80,91 @@ class Dot3d(VGroup):
 
 
 class Regular_Dodecahedron(VGroup):
+    def __init__(
+            self,
+            size=3,
+            vertex_size=0.2,
+            vertex_color=None,
+            edge_color=WHITE,
+            face_color=YELLOW,
+            edge_stroke=3,
+            face_opacity=0.25,
+            **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.size = size
+        self.vertex_size = vertex_size
+        self.vertex_color = vertex_color
+        self.edge_color = edge_color
+        self.face_color = face_color
+        self.edge_stroke = edge_stroke
+        self.face_opacity = face_opacity
 
-    CONFIG = {
-        'size': 3,
-        'vertex_size': 0.2,
-        'vertex_color': None,
-        'edge_color': WHITE,
-        'face_color': YELLOW,
-        'edge_stroke': 3,
-        'face_opacity': 0.25,
-    }
-
-    def __init__(self, **kwargs):
-        VGroup.__init__(self, **kwargs)
         self.create_vertices()
         self.create_edges()
         self.create_faces()
 
-        # self.add(self.vertices, self.edges, self.faces)
         self.add(self.faces, self.edges, self.vertices)
-
-        self.set_height(self.size)
-
-        # self.vertices.add_updater(self.vertex_direction_updater)
+        self.scale_to_fit_height(self.size)
 
     def create_vertices(self):
+        phi = (np.sqrt(5) + 1) / 2
 
-        phi = (np.sqrt(5)+1)/2
-        orange_points = np.array([[+1, +1, +1],
-                                  [+1, -1, +1],
-                                  [-1, -1, +1],
-                                  [-1, +1, +1],
-                                  [+1, +1, -1],
-                                  [+1, -1, -1],
-                                  [-1, -1, -1],
-                                  [-1, +1, -1],
-                                  ])
+        orange_points = np.array([
+            [+1, +1, +1],
+            [+1, -1, +1],
+            [-1, -1, +1],
+            [-1, +1, +1],
+            [+1, +1, -1],
+            [+1, -1, -1],
+            [-1, -1, -1],
+            [-1, +1, -1],
+        ])
 
-        green_points = np.array([[0, +phi, +1/phi],
-                                 [0, -phi, +1/phi],
-                                 [0, -phi, -1/phi],
-                                 [0, +phi, -1/phi],
-                                 ])
+        green_points = np.array([
+            [0, +phi, +1 / phi],
+            [0, -phi, +1 / phi],
+            [0, -phi, -1 / phi],
+            [0, +phi, -1 / phi],
+        ])
 
-        blue_points = np.array([[+1/phi, 0, +phi],
-                                [+1/phi, 0, -phi],
-                                [-1/phi, 0, -phi],
-                                [-1/phi, 0, +phi],
-                                ])
+        blue_points = np.array([
+            [+1 / phi, 0, +phi],
+            [+1 / phi, 0, -phi],
+            [-1 / phi, 0, -phi],
+            [-1 / phi, 0, +phi],
+        ])
 
-        red_points = np.array([[+phi, +1/phi, 0],
-                               [+phi, -1/phi, 0],
-                               [-phi, -1/phi, 0],
-                               [-phi, +1/phi, 0],
-                               ])
+        red_points = np.array([
+            [+phi, +1 / phi, 0],
+            [+phi, -1 / phi, 0],
+            [-phi, -1 / phi, 0],
+            [-phi, +1 / phi, 0],
+        ])
+
         self.O_dots, self.G_dots, self.B_dots, self.R_dots = VGroup(), VGroup(), VGroup(), VGroup()
 
-        for i in range(8):
-            color=ORANGE
-            if not self.vertex_color == None:
-                color = self.vertex_color
-            # dot_i = Dot(orange_points[i], color=color).set_height(self.vertex_size)
-            dot_i = Dot3d(orange_points[i], color=color, size=self.vertex_size)
-            self.O_dots.add(dot_i)
-        for i in range(4):
-            color = GREEN
-            if not self.vertex_color == None:
-                color = self.vertex_color
-            # dot_i = Dot(green_points[i], color=color).set_height(self.vertex_size)
-            dot_i = Dot3d(green_points[i], color=color, size=self.vertex_size)
-            self.G_dots.add(dot_i)
-        for i in range(4):
-            color=BLUE
-            if not self.vertex_color == None:
-                color = self.vertex_color
-            # dot_i = Dot(blue_points[i], color=color).set_height(self.vertex_size)
-            dot_i = Dot3d(blue_points[i], color=color, size=self.vertex_size)
-            self.B_dots.add(dot_i)
-        for i in range(4):
-            color=RED
-            if not self.vertex_color == None:
-                color = self.vertex_color
-            # dot_i = Dot(red_points[i], color=color).set_height(self.vertex_size)
-            dot_i = Dot3d(red_points[i], color=color, size=self.vertex_size)
-            self.R_dots.add(dot_i)
+        for point in orange_points:
+            color = ORANGE if self.vertex_color is None else self.vertex_color
+            dot = Dot3d(point, color=color, size=self.vertex_size)
+            self.O_dots.add(dot)
+
+        for point in green_points:
+            color = GREEN if self.vertex_color is None else self.vertex_color
+            dot = Dot3d(point, color=color, size=self.vertex_size)
+            self.G_dots.add(dot)
+
+        for point in blue_points:
+            color = BLUE if self.vertex_color is None else self.vertex_color
+            dot = Dot3d(point, color=color, size=self.vertex_size)
+            self.B_dots.add(dot)
+
+        for point in red_points:
+            color = RED if self.vertex_color is None else self.vertex_color
+            dot = Dot3d(point, color=color, size=self.vertex_size)
+            self.R_dots.add(dot)
+
         self.vertices = VGroup(self.O_dots, self.G_dots, self.B_dots, self.R_dots)
-        # self.vertices.set_height(self.size)
 
     def create_edges(self):
         def Line_by2dots(d1, d2, stroke_width=self.stroke_width, color=self.edge_color, **kwargs):
